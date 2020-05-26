e014a00bd9872fdc8f55e44cbac7e2eeacdd6589
eydodoyn <32019576+eydodoyn@users.noreply.github.com>
2020-05-25 22:50:27 -0700

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -43,6 +43,20 @@
     return obj;
   }
 
+  function renderTime(){
+  // Date
+  var mydate = new Date();
+  var year = mydate.getYear()
+    if (year < 1000){
+      year+=1900
+    }
+  var day = mydate.getDay();
+  var month = mydate.getMonth();
+  var daym = mydate.getDate();
+  var dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+  var montharray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+}
+                                       
   function _objectSpread(target) {
     for (var i = 1; i < arguments.length; i++) {
       var source = arguments[i] != null ? arguments[i] : {};
