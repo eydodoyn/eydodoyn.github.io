e4c2a395c6cc164f1923e7daf064f8f5ad7004df
eydodoyn <32019576+eydodoyn@users.noreply.github.com>
2020-05-25 19:37:23 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -59,3 +59,17 @@ exports.vendor = vendor;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
+
+function renderTime(){
+  // Date
+  var mydate = new Date();
+  var year = mydate.getYear()
+    if (year < 1000){
+      year+=1900
+    }
+  var day = mydate.getDay();
+  var month = mydate.getMonth();
+  var daym = mydate.getDate();
+  var dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+  var montharray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+}
